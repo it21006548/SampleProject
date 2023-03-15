b0875ff356d1be5b5422daa3336ae1f47fa23e5f
User <it21006548@my.sliit.lk>
2023-03-15 08:57:09 +0530

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+//Routes
+app.get("/", (req, res) => {
+    res.send("Home page");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
